fe183598a4a36b44fe26d6232dbbdb71d802770d
Unknown <luke.towell@hotmail.com>
2018-02-25 14:07:24 +0000

    Finished application
    finished application with method for generating and storing random numbers for the selection of the ball images. 
    
    Also includes the use of the shake motion activiation on the application.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    let ballArray : [String] = ["ball1","ball2","ball3","ball4","ball5"]
+    var randomImageIndex : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballSelect()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype,
+                     with event: UIEvent?){
+        ballSelect()
+    }
+    
+    func ballSelect(){
+        randomImageIndex = Int(arc4random_uniform(5))
+        ballImage.image = UIImage(named: ballArray[randomImageIndex])
+        
+    }
+    
+    
+    
 }
 
